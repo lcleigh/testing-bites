2ecbe0802f25e965e81d92b2947cd0b18c4293af
laura.leigh <lauracleigh314@gmail.com>
2022-10-03 11:38:09 +0100

    adding more tests to counter spec

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,11 +1,34 @@
 require "counter"
 
 RSpec.describe Counter do
-    it "adds on a given number" do
+    it "initially reports a count of zero" do
+        counter = Counter.new
+        result = counter.report()
+        expect(result).to eq "Counted to 0 so far."
+    end
+
+    it "reports a count of 12" do
         counter = Counter.new
         counter.add(12)
         result = counter.report()
         expect(result).to eq "Counted to 12 so far."
     end
 
+    it "reports a count of all added values" do
+        counter = Counter.new
+        counter.add(6)
+        counter.add(21)
+        counter.add(8)
+        result = counter.report()
+        expect(result).to eq "Counted to 35 so far."
+    end
+
+    it "reports a count of all added values" do
+        counter = Counter.new
+        counter.add(-6)
+        counter.add(4)
+        counter.add(5.5)
+        result = counter.report()
+        expect(result).to eq "Counted to 3.5 so far."
+    end
 end
